3ed6831b9bc8cb52e09df6d780fc6b9852d937af
nksm-npe <notpoorelephant@gmail.com>
2025-04-20 19:31:17 +0900

    tmp

--- src/extension.js
+++ src/extension.js
@@ -1,11 +1,21 @@
-// 拡張機能が有効化されたときに呼び出される関数
+/**
+ * Qmonus YAML Highlighter拡張機能のエントリーポイント
+ * @param {import('vscode').ExtensionContext} context - 拡張機能のコンテキスト
+ */
 function activate(context) {
   console.log('Qmonus YAML Highlighter が有効化されました');
+  // シンタックスハイライト拡張機能は、TextMate文法定義ファイルで動作するため
+  // ここでは追加のコードは必要ありません
 }
 
-// 拡張機能が無効化されるときに呼び出される関数
-function deactivate() {}
+/**
+ * 拡張機能が無効化されるときに呼び出される関数
+ */
+function deactivate() {
+  // クリーンアップが必要な場合はここに記述
+}
 
+// CommonJSモジュールのエクスポート
 module.exports = {
   activate,
   deactivate
